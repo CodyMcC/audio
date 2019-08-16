f8fbfc48200a8b5ae5fdae23575cdb0bef2283e9
codymcco <codymccomber@gmail.com>
2019-08-15 22:12:08 -0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -75,11 +75,11 @@ setup(
 
     # For example, the following would provide a command called `sample` which
     # executes the function `main` from this package when invoked:
-    entry_points={  # Optional
-        'console_scripts': [
-            'audio_bars = AudioProcessing.__main__:main',
-        ],
-    },
+    # entry_points={  # Optional
+    #     'console_scripts': [
+    #         'audio_bars = AudioProcessing.__main__:main',
+    #     ],
+    # },
 
     # List additional URLs that are relevant to your project as a dict.
     #
